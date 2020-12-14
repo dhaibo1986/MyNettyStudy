3f8c3d637be12d4c3ac7519ca62b5ba28ce824c5
haibo.duan <dhaibo1986@sohu.com>
2020-12-14 21:02:39 +0800

    BIO 代码 网络编程难点，异常处理，网络关闭

---
+++ src/main/java/com/dhb/nettystudy/bio/Client.java
@@ -0,0 +1,16 @@
+package com.dhb.nettystudy.bio;
+
+import java.net.Socket;
+
+public class Client {
+	public static void main(String[] args) throws Exception{
+		Socket s = new Socket("127.0.0.1",8888);
+		s.getOutputStream().write("HelloServer".getBytes());
+		s.getOutputStream().flush();
+		System.out.println("write over,waiting for msg back ...");
+		byte[] bytes = new byte[1024];
+		int len = s.getInputStream().read(bytes);
+		System.out.println(new String(bytes,0,len));
+		s.close();
+	}
+}
