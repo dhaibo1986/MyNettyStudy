7015b6cab29cc48a5501efcc3ffead5e08598f6e
haibo.duan <dhaibo1986@sohu.com>
2020-12-30 20:29:01 +0800

    Netty实现的server端

--- src/main/java/com/dhb/nettystudy/s01/Server.java
+++ src/main/java/com/dhb/nettystudy/s01/Server.java
@@ -1,15 +1,40 @@
 package com.dhb.nettystudy.s01;
 
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 public class Server {
 
-	public static void main(String[] args) throws Exception{
-		ServerSocket ss = new ServerSocket();
-		ss.bind(new InetSocketAddress(8888));
-		Socket s = ss.accept();
-		System.out.println("a client connect !");
+	public static void main(String[] args) {
+		EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+		EventLoopGroup workerGroup = new NioEventLoopGroup(2);
+
+		ServerBootstrap b = new ServerBootstrap();
+		try {
+			ChannelFuture f = b.group(bossGroup,workerGroup)
+					.channel(NioServerSocketChannel.class)
+					.childHandler(new ChannelInitializer<SocketChannel>() {
+
+						@Override
+						protected void initChannel(SocketChannel ch) throws Exception {
+							System.out.println(ch);
+						}
+					})
+					.bind(8888)
+					.sync();
+			System.out.println("server started!");
+			f.channel().closeFuture().sync();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			workerGroup.shutdownGracefully();
+			bossGroup.shutdownGracefully();
+		}
+
 	}
 }
