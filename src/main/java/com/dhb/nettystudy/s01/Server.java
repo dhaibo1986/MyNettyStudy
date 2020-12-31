158bee22d592d323256a33e4d34327a123c25317
haibo.duan <dhaibo1986@sohu.com>
2020-12-31 20:05:24 +0800

    接收消息handler的处理，需要注意的是此处用到了ByteBuf 对直接内存的引用和回收

--- src/main/java/com/dhb/nettystudy/s01/Server.java
+++ src/main/java/com/dhb/nettystudy/s01/Server.java
@@ -1,10 +1,13 @@
 package com.dhb.nettystudy.s01;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.ReferenceCountUtil;
 
 public class Server {
 
@@ -20,10 +23,8 @@ public class Server {
 
 						@Override
 						protected void initChannel(SocketChannel ch) throws Exception {
-							System.out.println(ch);
 							ChannelPipeline pipeline = ch.pipeline();
 							pipeline.addLast(new ServerChildHandler());
-							System.out.println(Thread.currentThread().getId());
 						}
 					})
 					.bind(8888)
@@ -43,12 +44,18 @@ class ServerChildHandler extends ChannelInboundHandlerAdapter {
 
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-
-		System.out.println(msg.toString());
+		ByteBuf buf = null;
+		try {
+			buf = (ByteBuf)msg;
+			System.out.println("==============================");
+			System.out.println(buf.refCnt());
+			System.out.println(buf.toString());
+		} finally {
+			if(buf != null) {
+				ReferenceCountUtil.release(buf);
+				System.out.println(buf.refCnt());
+			}
+		}
 	}
 
-	@Override
-	public void channelActive(ChannelHandlerContext ctx) throws Exception {
-		System.out.println(Thread.currentThread().getId());
-	}
 }
