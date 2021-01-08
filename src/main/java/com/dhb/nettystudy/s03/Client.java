362df4a2ee2ea40fc813a4830675c865763a4d18
haibo.duan <dhaibo1986@sohu.com>
2021-01-08 18:36:39 +0800

    从Frame启动，并发送hello到server端

--- src/main/java/com/dhb/nettystudy/s03/Client.java
+++ src/main/java/com/dhb/nettystudy/s03/Client.java
@@ -1,44 +1,76 @@
 package com.dhb.nettystudy.s03;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
-import io.netty.channel.EventLoopGroup;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.ReferenceCountUtil;
 
-public class Client {
 
-		public void connect() {
-			EventLoopGroup group = new NioEventLoopGroup(1);
-			Bootstrap b = new Bootstrap();
-
-			try {
-				ChannelFuture f = b.group(group).channel(NioSocketChannel.class)
-						.connect("localhost",8888);
-				f.addListener(new ChannelFutureListener() {
-					@Override
-					public void operationComplete(ChannelFuture future) throws Exception {
-						if(!future.isSuccess()) {
-							System.out.println("not connect!");
-						}else {
-							System.out.println("connected!");
-						}
-					}
-				});
-				f.sync();
-				System.out.println("...");
-				f.channel().closeFuture().sync();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			} finally {
-					group.shutdownGracefully();
-			}
-		}
+public  class Client {
 
 	public static void main(String[] args) {
 		Client c = new Client();
 		c.connect();
 	}
 
+	public void connect() {
+		EventLoopGroup group = new NioEventLoopGroup(1);
+		Bootstrap b = new Bootstrap();
+
+		try {
+			ChannelFuture f = b.group(group).channel(NioSocketChannel.class)
+					.handler(new ChannelInitializer<SocketChannel>() {
+						@Override
+						protected void initChannel(SocketChannel ch) throws Exception {
+							ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+								@Override
+								public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+									ByteBuf buf = null;
+									try {
+										buf = (ByteBuf) msg;
+										byte[] bytes = new byte[buf.readableBytes()];
+										buf.getBytes(buf.readerIndex(), bytes);
+										System.out.println(new String(bytes));
+									} finally {
+										if (buf != null) {
+											ReferenceCountUtil.release(buf);
+										}
+									}
+								}
+
+								@Override
+								public void channelActive(ChannelHandlerContext ctx) throws Exception {
+									//channel第一次连接上之后写出一个字符
+									ByteBuf buf = Unpooled.copiedBuffer("hello".getBytes());
+									ctx.writeAndFlush(buf);
+								}
+							});
+						}
+					})
+					.connect("localhost", 8888);
+			f.addListener(new ChannelFutureListener() {
+				@Override
+				public void operationComplete(ChannelFuture future) throws Exception {
+					if (!future.isSuccess()) {
+						System.out.println("not connect!");
+					} else {
+						System.out.println("connected!");
+					}
+				}
+			});
+			f.sync();
+			System.out.println("...");
+			f.channel().closeFuture().sync();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			group.shutdownGracefully();
+		}
+	}
+
 }
+
