587e996a320be22681151552ab2478e1870ac564
haibo.duan <dhaibo1986@sohu.com>
2021-01-11 19:25:58 +0800

    优雅的断开连接，再客户端发送_bye_ 服务端收到这个字符串之后，关闭连接。

---
+++ src/main/java/com/dhb/nettystudy/s10/ClientFrame.java
@@ -0,0 +1,65 @@
+package com.dhb.nettystudy.s10;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class ClientFrame  extends Frame {
+
+	public static final ClientFrame INSTANCE = new ClientFrame();
+
+	public static final String LINE_SEP = System.getProperty("line.separator","\n");
+
+	TextArea ta = new TextArea();
+	TextField tf = new TextField();
+	Client client = null;
+
+	public ClientFrame() {
+		this.setSize(600,400);
+		this.setLocation(100,20);
+		this.setVisible(true);
+		this.add(ta,BorderLayout.CENTER);
+		this.add(tf,BorderLayout.SOUTH);
+		tf.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				client.send(tf.getText());
+//				ta.setText(ta.getText()+"\r\n"+tf.getText());
+				tf.setText("");
+
+			}
+		});
+
+		//增加关闭按钮事件
+		this.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				client.closeConnect();
+				System.exit(0);
+			}
+		});
+	}
+
+	public void updateText(String text) {
+		ta.setText(ta.getText()+LINE_SEP+text);
+	}
+
+
+	private void connectToServer() {
+		client = new Client();
+		client.connect();
+	}
+
+	public static void start() {
+		ClientFrame frame = ClientFrame.INSTANCE;
+		frame.setVisible(true);
+		frame.connectToServer();
+	}
+
+	public static void main(String[] args) {
+		ClientFrame.start();
+	}
+
+}
