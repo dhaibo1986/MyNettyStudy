b42eb240154af3f9028f47a9605e07cb80c20076
haibo.duan <dhaibo1986@sohu.com>
2021-01-11 19:47:39 +0800

    客户端优雅的退出，同时发送_bye_也能关闭客户端Frame

---
+++ src/main/java/com/dhb/nettystudy/s11/ServerFrame.java
@@ -0,0 +1,23 @@
+package com.dhb.nettystudy.s11;
+
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+
+import java.awt.*;
+
+public class ServerFrame extends Frame {
+
+	public static final ServerFrame INSTANCE = new ServerFrame();
+
+	Button btnStart = new Button("start");
+	TextArea taLeft = new TextArea();
+	TextArea taRight = new TextArea();
+	Server server = new Server();
+
+	public ServerFrame() {
+		EventLoopGroup group = new NioEventLoopGroup(1);
+
+
+	}
+
+}
