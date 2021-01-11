1fe7a40e1c765cecb8274d9ecf3b7b569d3b1b92
haibo.duan <dhaibo1986@sohu.com>
2021-01-11 19:19:04 +0800

    增加客户端关闭的方法

---
+++ src/main/java/com/dhb/nettystudy/s09/Client.java
@@ -0,0 +1,101 @@
+package com.dhb.nettystudy.s09;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.ReferenceCountUtil;
+
+
+public class Client {
+
+	private Channel channel = null;
+
+	private Client c = null;
+
+	private EventLoopGroup group;
+
+	public static void main(String[] args) {
+		ClientFrame.start();
+	}
+
+	public void connect() {
+		group = new NioEventLoopGroup(1);
+		Bootstrap b = new Bootstrap();
+
+		try {
+			ChannelFuture f = b.group(group).channel(NioSocketChannel.class)
+					.handler(new ChannelInitializer<SocketChannel>() {
+						@Override
+						protected void initChannel(SocketChannel ch) throws Exception {
+							ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+								@Override
+								public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+									ByteBuf buf = null;
+									try {
+										buf = (ByteBuf) msg;
+										byte[] bytes = new byte[buf.readableBytes()];
+										buf.getBytes(buf.readerIndex(), bytes);
+										String msgAccepted = new String(bytes);
+										ClientFrame.INSTANCE.updateText(msgAccepted);
+
+									} finally {
+										if (buf != null) {
+											ReferenceCountUtil.release(buf);
+										}
+									}
+								}
+								@Override
+								public void channelActive(ChannelHandlerContext ctx) throws Exception {
+									//channel第一次连接上之后写出一个字符
+									ByteBuf buf = Unpooled.copiedBuffer("hello".getBytes());
+									ctx.writeAndFlush(buf);
+								}
+							});
+						}
+					})
+					.connect("localhost", 8888);
+			f.addListener(new ChannelFutureListener() {
+				@Override
+				public void operationComplete(ChannelFuture future) throws Exception {
+					if (!future.isSuccess()) {
+						System.out.println("not connect!");
+					} else {
+						System.out.println("connected!");
+						channel = future.channel();
+					}
+				}
+			});
+			f.sync();
+			System.out.println("...");
+			f.channel().closeFuture().sync();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			group.shutdownGracefully();
+		}
+	}
+
+	public void closeConnect() {
+		this.send("_bye_");
+		this.channel.close();
+	}
+
+	public void stop() {
+		this.channel.flush();
+		this.channel.closeFuture();
+		this.group.shutdownGracefully();
+	}
+
+	public void send(String msg) {
+		ByteBuf buf = Unpooled.copiedBuffer(msg.getBytes());
+		channel.writeAndFlush(buf);
+	}
+
+
+
+}
+
---
+++ src/main/java/com/dhb/nettystudy/s09/Server.java
@@ -0,0 +1,85 @@
+package com.dhb.nettystudy.s09;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+public class Server {
+
+	public static ChannelGroup clients = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
+	public static void main(String[] args) {
+		EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+		EventLoopGroup workerGroup = new NioEventLoopGroup(2);
+
+		ServerBootstrap b = new ServerBootstrap();
+		try {
+			ChannelFuture f = b.group(bossGroup,workerGroup)
+					.channel(NioServerSocketChannel.class)
+					.childHandler(new ChannelInitializer<SocketChannel>() {
+
+						@Override
+						protected void initChannel(SocketChannel ch) throws Exception {
+							ChannelPipeline pipeline = ch.pipeline();
+							pipeline.addLast(new ServerChildHandler());
+						}
+					})
+					.bind(8888)
+					.sync();
+			System.out.println("server started!");
+			f.channel().closeFuture().sync();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			workerGroup.shutdownGracefully();
+			bossGroup.shutdownGracefully();
+		}
+
+	}
+}
+
+class ServerChildHandler extends ChannelInboundHandlerAdapter {
+
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		Server.clients.add(ctx.channel());
+	}
+
+	@Override
+	public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("channel ["+ctx.name()+"] 连接关闭，被移除。");
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		cause.printStackTrace();
+		Server.clients.remove(ctx.channel());
+		ctx.close();
+	}
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		ByteBuf buf = null;
+		if(msg instanceof ByteBuf) {
+			try {
+				buf = (ByteBuf) msg;
+				System.out.println("==============================");
+				byte[] bytes = new byte[buf.readableBytes()];
+				buf.getBytes(buf.readerIndex(), bytes);
+				System.out.println("server receive:"+new String(bytes));
+				Server.clients.writeAndFlush(msg);
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+
+			}
+		}
+	}
+
+}
